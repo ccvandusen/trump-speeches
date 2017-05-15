bc8e576bdb86556f38e0fce5cdcb65611d0fab4f
ChrisV <chris.vandusen1@gmail.com>
2017-05-15 16:46:22 -0700

    adding R code

---
+++ trump_wordcloud.R
@@ -0,0 +1,20 @@
+
+trump <- Corpus(DirSource("/Users/ChrisV/Documents/Blog_files/trump_speeches-master/wordcloud/"))
+trump_clean <- tm_map(trump, content_transformer(stripWhitespace))
+trump_clean <- tm_map(trump_clean, content_transformer(tolower))
+trump_clean <- tm_map(trump_clean, content_transformer(removePunctuation))
+trump_clean <- tm_map(trump_clean, content_transformer(stemDocument))
+trump_clean <- tm_map(trump_clean, removeWords, c(stopwords("english"),"applause","will","millions"))
+
+png("wordcloud.png", width=1280,height=800)
+wordcloud(trump_clean, scale=c(4,0.7), max.words=100, random.order=FALSE, rot.per=0.35, use.r.layout=FALSE, colors=brewer.pal(9, "Set1"))
+
+hillary <- Corpus(DirSource("/Users/ChrisV/Documents/Blog_files/hillaryspeech1/"))
+hillary_clean <- tm_map(hillary, content_transformer(stripWhitespace))
+hillary_clean <- tm_map(hillary_clean, content_transformer(tolower))
+hillary_clean <- tm_map(hillary_clean, content_transformer(removePunctuation))
+hillary_clean <- tm_map(hillary_clean, content_transformer(stemDocument))
+hillary_clean <- tm_map(hillary_clean, removeWords, c(stopwords("english"),"applause","will","millions"))
+
+wordcloud(hillary_clean, scale=c(3,0.5), max.words=100, random.order=FALSE, rot.per=0.35, use.r.layout=FALSE, colors=brewer.pal(9, "Set1"))
+
